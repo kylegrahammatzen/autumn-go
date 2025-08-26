04f8297205e514d4fa0c5a53e62f1b336e2cd17f
Kyle <kgrahammatzen@gmail.com>
2025-08-26 11:36:45 -0400

    feat(sdk): add customer management types to main package exports

--- autumn.go
+++ autumn.go
@@ -18,6 +18,16 @@ type CustomerData = autumn.CustomerData
 type Feature = autumn.Feature
 type AttachOption = autumn.AttachOption
 
+type CreateCustomerOptions = autumn.CreateCustomerOptions
+type UpdateCustomerOptions = autumn.UpdateCustomerOptions
+type SetBalanceOptions = autumn.SetBalanceOptions
+type FeatureBalance = autumn.FeatureBalance
+type BillingPortalOptions = autumn.BillingPortalOptions
+type CustomerResponse = autumn.CustomerResponse
+type BillingPortalResponse = autumn.BillingPortalResponse
+type CustomerProduct = autumn.CustomerProduct
+type CustomerFeature = autumn.CustomerFeature
+
 func NewClient(token string) *Client {
 	return autumn.NewClient(token)
 }
