2b1f11ba6ffc9223c7362a282e3077c727735e88
Kyle <kgrahammatzen@gmail.com>
2025-08-26 10:53:48 -0400

    refactor(examples): update to use new package structure

--- examples/main.go
+++ examples/main.go
@@ -4,29 +4,27 @@ import (
 	"fmt"
 	"log"
 
-	autumn "github.com/kylegrahammatzen/autumn-go"
+	"github.com/kylegrahammatzen/autumn-go"
 )
 
 func main() {
 	client := autumn.NewClient("am_sk_test_XESp2wyPE...")
 
-	attachReq := autumn.AttachRequest{
-		CustomerID: "john_doe",
-		ProductID:  autumn.StringPtr("chat_messages"),
+	attachOptions := autumn.AttachOptions{
+		ProductID: autumn.StringPtr("chat_messages"),
 	}
 
-	attachResp, err := client.Attach(attachReq)
+	attachResp, err := client.Attach("john_doe", attachOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("Attached customer: %+v\n", attachResp)
 
-	checkReq := autumn.CheckRequest{
-		CustomerID: "john_doe",
-		ProductID:  autumn.StringPtr("chat_messages"),
+	checkOptions := autumn.CheckOptions{
+		ProductID: autumn.StringPtr("chat_messages"),
 	}
 
-	checkResp, err := client.Check(checkReq)
+	checkResp, err := client.Check("john_doe", checkOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,19 +33,18 @@ func main() {
 		fmt.Println("Sending chat message...")
 	}
 
-	trackReq := autumn.TrackRequest{
-		CustomerID: "john_doe",
-		FeatureID:  autumn.StringPtr("chat_messages"),
-		Value:      1,
+	trackOptions := autumn.TrackOptions{
+		FeatureID: autumn.StringPtr("chat_messages"),
+		Value:     1,
 	}
 
-	trackResp, err := client.Track(trackReq)
+	trackResp, err := client.Track("john_doe", trackOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("Tracked usage: %+v\n", trackResp)
 
-	checkResp2, err := client.Check(checkReq)
+	checkResp2, err := client.Check("john_doe", checkOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
